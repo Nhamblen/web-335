02769fceb000764b19884430b785c9e57861b700
Noah <nhamblen21@gmail.com>
2025-06-04 22:24:07 -0500

    Rename file and add quote printing

---
+++ week_2/Hamblen_quote.py
@@ -0,0 +1,9 @@
+"""
+Author: Noah Hamblen
+Date: 6/4/25
+File Name: Hamblen_quote.py
+Description: This program prints out an inspirational quote and credits the original author.
+"""
+
+# Printing a motivational quote and its author
+print('"Sometimes you never realize the value of a moment until it becomes a memory" - Dr. Seuss')
