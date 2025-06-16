b6c3e6444ac9a927df286a4ae3fc08b918c4ae42
Noah <nhamblen21@gmail.com>
2025-06-15 22:06:15 -0500

    Expand on comments

--- week_4/Noah_Hamblen_Exercise 4.3.js
+++ week_4/Noah_Hamblen_Exercise 4.3.js
@@ -1,17 +1,17 @@
 // Noah Hamblen
 // 6/15/25
 
-// a. Display all users in the collection
+// a. Display all users in the collection (with pretty format for readability)
 db.users.find().pretty();
 
-// b. Display the user with the email address jbach@me.com
+// b. Display the user with the email address jbach@me.com (with pretty format for readability)
 db.users.find({ email: "jbach@me.com" }).pretty();
 
-// c. Display the user with the last name Mozart
+// c. Display the user with the last name Mozart (with pretty format for readability)
 db.users.find({ lastName: "Mozart" }).pretty();
 
-// d. Display the user with the first name Richard
+// d. Display the user with the first name Richard (with pretty format for readability)
 db.users.find({ firstName: "Richard" }).pretty();
 
-// e. Display the user with employeeId 1010
+// e. Display the user with employeeId 1010 (with pretty format for readability)
 db.users.find({ employeeId: "1010" }).pretty();
