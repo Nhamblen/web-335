5ec0e23685ebfda5fe39077d96fcd07fa132a9ca
Noah <nhamblen21@gmail.com>
2025-06-30 20:57:08 -0500

    Add two queries for assignment

--- week_6/Hamblen_usersp1.py
+++ week_6/Hamblen_usersp1.py
@@ -15,7 +15,16 @@ client = MongoClient("mongodb+srv://web335_user:s3cret@cluster0.lujih.mongodb.ne
 db = client['web335DB']
 
 # Display all documents in the users collection
-print("\n-- Display All Users --")
+print("\n-- Displaying All Users --")
 for user in db.users.find({}, {"_id": 0, "firstName": 1, "lastName": 1}):
     print(user)
 
+# Display a document where the employeeId is 1011
+print("\n-- Displaying User With Employee ID 1011 --")
+user_1011 = db.users.find_one({"employeeId": "1011"})
+print(user_1011)
+
+# Display a document where the lastName is Mozart
+print("\n-- Displaying User With Last Name Mozart --")
+user_mozart = db.users.find_one({"lastName": "Mozart"})
+print(user_mozart)
